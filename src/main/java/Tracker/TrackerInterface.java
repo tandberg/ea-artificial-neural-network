e7f4a91bf89c4bd9e4e2cd35f6c18a2d2b330676
Øystein Tandberg <tandeey@gmail.com>
2014-03-23 18:59:16 +0100

    tracker world

---
+++ src/main/java/Tracker/TrackerInterface.java
@@ -0,0 +1,7 @@
+package tracker;
+
+import java.util.*;
+
+public interface TrackerInterface {
+	public List getEnvironment();
+}
---
+++ src/main/java/Tracker/TrackerWorld.java
@@ -0,0 +1,195 @@
+package tracker;
+
+import java.util.*;
+import java.io.*;
+public class TrackerWorld implements TrackerInterface {
+
+	private final static int AGENT_SIZE = 5;
+	private final static int SHADOW_SENSORS = 5;
+	
+	private final static int FALLING_MIN = 1;
+	private final static int FALLING_MAX = 5;
+
+	private final static int ARENA_WIDTH = 30;
+	private final static int ARENA_HEIGHT = 15;
+
+	private int timestamp;
+	private int agentLeftPos;
+	private int enemyLeftPos;
+	private int enemyY;
+	private int enemySize;
+	private int[][] arena;
+
+	public TrackerWorld() {
+		timestamp = 0;
+		arena = new int[ARENA_WIDTH][ARENA_HEIGHT];
+	}
+
+	public void setAgent(int leftPos) {
+		this.agentLeftPos = leftPos;
+		for(int i = leftPos; i < leftPos + AGENT_SIZE; i++) {
+			int pos = i % ARENA_WIDTH;
+			arena[pos][ARENA_HEIGHT-1] = 1;
+		}
+	}
+
+	public void doMove(int dx) {
+		if(this.agentLeftPos == -1) {
+			this.agentLeftPos = ARENA_WIDTH+1;
+		}
+
+		if(dx == 1) {
+			arena[Math.abs(this.agentLeftPos % ARENA_WIDTH)][ARENA_HEIGHT-1] = 0;
+			arena[Math.abs((this.agentLeftPos+AGENT_SIZE) % ARENA_WIDTH)][ARENA_HEIGHT-1] = 1;
+		} else if(dx == -1) {
+			System.out.println(agentLeftPos);
+			System.out.println(Math.abs((this.agentLeftPos-1) % ARENA_WIDTH));
+			System.out.println(Math.abs((this.agentLeftPos+AGENT_SIZE-1) % ARENA_WIDTH)+"\n\n");
+
+			arena[Math.abs((this.agentLeftPos-1) % ARENA_WIDTH)][ARENA_HEIGHT-1] = 1;
+			arena[Math.abs((this.agentLeftPos+AGENT_SIZE-1) % ARENA_WIDTH)][ARENA_HEIGHT-1] = 0;
+		}
+
+		this.agentLeftPos += dx;
+		clocktick();
+	}
+
+	public void clocktick() {
+		if(timestamp % ARENA_HEIGHT == 0) {
+			addRandomEnemy();
+		}
+
+		updateFallingBlocks();
+		timestamp += 1;
+	}
+
+	public void addRandomEnemy() {
+		enemySize = (int) (Math.random() * 6);
+		enemyLeftPos = (int) (Math.random() * ARENA_WIDTH);
+		enemyY = 0;
+	}
+
+	public void updateFallingBlocks() {
+		try{
+			for(int i = enemyLeftPos; i < enemyLeftPos+enemySize; i++) {
+				arena[i][enemyY] = 0;
+			}
+
+
+			for(int i = enemyLeftPos; i < enemyLeftPos+enemySize; i++) {
+				arena[i][enemyY] = 2;
+			}
+			enemyY += 1;
+
+		} catch(ArrayIndexOutOfBoundsException e) {
+
+		}
+		
+
+	}
+
+	public List getEnvironment() {
+		return null;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		for(int i = 0; i < ARENA_HEIGHT; i++) {
+			sb.append("[");
+			for(int j = 0; j < ARENA_WIDTH; j++) {
+				sb.append(arena[j][i]);
+				if(j != ARENA_WIDTH-1) {
+					sb.append(",");
+				}
+			}
+			sb.append("]");
+			if(i != ARENA_HEIGHT-1) {
+				sb.append(",");
+			}
+		}
+		sb.append("]");
+
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		TrackerWorld w = new TrackerWorld();
+		w.setAgent(6);
+
+		ArrayList<String> pelle = new ArrayList<String>();
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+		pelle.add(w.toString());
+		w.doMove(-1);
+
+		pelle.add(w.toString());
+		w.doMove(1);
+		pelle.add(w.toString());
+		w.doMove(1);
+		pelle.add(w.toString());
+		w.doMove(1);
+		pelle.add(w.toString());
+		w.doMove(1);
+		pelle.add(w.toString());
+		w.doMove(1);
+		pelle.add(w.toString());
+		w.doMove(1);
+		pelle.add(w.toString());
+		w.doMove(1);
+		pelle.add(w.toString());
+		w.doMove(1);
+		pelle.add(w.toString());
+		w.doMove(1);
+		pelle.add(w.toString());
+		w.doMove(1);
+		pelle.add(w.toString());
+		w.doMove(1);
+		pelle.add(w.toString());
+		w.doMove(1);
+		pelle.add(w.toString());
+		w.doMove(1);
+		try {
+
+ String current = new java.io.File( "." ).getCanonicalPath();
+        System.out.println("Current dir:"+current);
+
+			PrintWriter writer = new PrintWriter("src/main/javascript/tracker/output.txt", "UTF-8");
+			writer.println(pelle.toString());
+			writer.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		// System.out.println(pelle);
+	}
+
+}
