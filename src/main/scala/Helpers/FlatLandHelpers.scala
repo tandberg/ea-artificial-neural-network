16474de3a48c3ef07e13e4bf26cb2c340b7dfe35
Øystein Tandberg <tandeey@gmail.com>
2014-03-27 16:35:20 +0100

    Weights is now in the -1 to 1 space

--- src/main/scala/Helpers/FlatLandHelpers.scala
+++ src/main/scala/Helpers/FlatLandHelpers.scala
@@ -4,27 +4,35 @@ import scala.util.Random
 
 object FlatLandHelpers {
 
+	val bitstringpercision = 8
+	val numberOfWeights = 12
+
 	def generateRandomBitString = {
-		val length = 12 * 4
+
+		val length = numberOfWeights * (bitstringpercision+1)
 		var bitString = Array[Int]() 
 		val random = new Random()
-		for (i <- 0 to length) {
+		for (i <- 0 to length-1) {
 			bitString = bitString :+ random.nextInt(2)	
 		}
 		bitString
 	}
 
 	def bitStringToWeights(array: Array[Int], bitstringpercision: Int):Array[Double]= {
-		val split = array.grouped(bitstringpercision).toArray
+		val split = array.grouped(bitstringpercision+1).toArray
 		var weights:Array[Double] = Array()
+
 
 		for(i <- 0 to split.length - 1) {
 			var weight = 0.0
-			for(j <- 0 to split(i).length - 1) {
+			for(j <- 1 to split(i).length - 1) {
 
 				weight += (split(i)(j) << (split(i).length - j - 1))
 			}
-			weight /= math.pow(2, bitstringpercision)
+			weight /= math.pow(2, bitstringpercision)-1
+			if(split(i)(0) == 1) {
+				weight *= -1;
+			}
 			weights = weights :+ weight
 		}
 		weights
@@ -61,5 +69,4 @@ object FlatLandHelpers {
 		}
 		array map (flip _)
 	}
-
 }
